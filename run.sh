d8990f2f0a8d0f8e59995b8255594d8925523b93
salllmanshaik <hello.salmanshaik@gmail.com>
2025-05-31 14:43:17 +0530

    docker

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-echo java -XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=90 -javaagent:/home/java/newrelic/newrelic.jar -Dnewrelic.config.license_key=${NEW_RELIC_LICENSE_KEY}  -Dnewrelic.config.app_name=shipping -jar /home/java/shipping.jar > command
-bash command
+echo java -XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=90 -javaagent:/app/newrelic/newrelic.jar -Dnewrelic.config.license_key=${NEW_RELIC_LICENSE_KEY}  -Dnewrelic.config.app_name=shipping -jar /app/shipping.jar > command
+#bash -x command
+java -XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=90 -javaagent:/app/newrelic/newrelic.jar -Dnewrelic.config.license_key=${NEW_RELIC_LICENSE_KEY}  -Dnewrelic.config.app_name=shipping -jar /app/shipping.jar
